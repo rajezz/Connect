23549f511c7167bfeef64a9ea736c1e3f457b6fd
Rajeswaran <rajezzandrj@gmail.com>
2020-07-09 12:16:06 +0530

    added dynamic port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mySql = require('mysql');
 const path = require('path');
 const faker = require('faker');
 const router = require('./router/route');
-
+var port = process.env.PORT || 3000;
 var app = express();
 app.use(body_parser.urlencoded({extended: true,}));
 app.use(body_parser.json());
@@ -13,4 +13,4 @@ app.use(body_parser.json());
 app.use('/', router);
 app.use(express.static(path.join(__dirname + '/public')));
 
-app.listen(3000);
+app.listen(port);
