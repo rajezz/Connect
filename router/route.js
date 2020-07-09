fdc285421581e3d17ad06f45066dc0c87904d2a9
Rajeswaran <rajezzandrj@gmail.com>
2020-07-09 13:20:44 +0530

    resolved issue in backend

--- router/route.js
+++ router/route.js
@@ -38,7 +38,7 @@ router.get('/login', function (req, res) {
 
     if (user_mail && password) {
         connection.query('select * from users where email = ? and password = ?', [user_mail, password], (err, result, field) => {
-            if (result.length > 0) {
+            if (result && result.length > 0) {
                 let userdetail = {
                     email: result[0].email,
                     username: result[0].username,
@@ -75,7 +75,7 @@ router.get('/register-user', function (req, res) {
         connection.query('select * from users where email = ?', [user_details.email], (err, result, field) => {
             if (err) {
                 res.send({ message: 'something went wrong' });
-            } else if (result.length > 0) {
+            } else if (result && result.length > 0) {
                 res.send({ message: 'email exists' });
             } else {
                 connection.query('insert into  users (email, password, profile_pic, dob, phone_no, address, username) values (?, ?, ?, ?, ?, ?, ?)', [user_details.email, user_details.password, user_details.profile_pic, user_details.dob, user_details.phone_no, user_details.address, user_details.username], (err, result, field) => {
